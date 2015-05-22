b75748ae0c9568535778e94b98470b828c06f722
Emrhys <thenine@gmail.com>
2015-05-22 12:03:21 -0700

    Created unit test scenarios
    created the unit test scenarios for @media, @font-face, and HTML
    elements.

--- premailer/metadata_test.py
+++ premailer/metadata_test.py
@@ -2,9 +2,53 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    """ This section of unit tests is for @media queries """
+    def test_standard_media_query(self):
+    # self.assertEqual(True, False)
 
+    def test_media_query_with_screen(self):
+
+    def test_media_query_with_print(self):
+
+    def test_media_query_with_max_width(self):
+
+    def test_media_query_with_min_width(self):
+
+    def test_media_query_with_projection(self):
+
+    def test_multiple_media_queries_in_one_style_tag(self):
+
+    def test_multiple_media_queries_in_two_style_tags(self):
+
+    def test_blank_media_query(self):
+
+    def test_invalid_media_selector(self):
+
+    def test_nested_media_query(self):
+
+        """This section of unit tests is for @Font-Face """
+    def test_standard_font_face(self):
+
+    def test_multiple_font_face_in_one_style_tag(self):
+
+    def test_multiple_font_face_in_two_style_tags(self):
+
+    def test_invalid_font_face_property_name(self):
+
+    """ This section of unit tests is for HTML elements"""
+
+    def test_standard_button_tag(self):
+
+    def test_input_button_tag(self):
+
+    def test_script_tag(self):
+
+    def test_style_tag(self):
+
+
+    """This sectiion of unit tests is for miscellaneous scenarios"""
+
+    def test_multiple_new_lines_in_css_rule(self):
 
 if __name__ == '__main__':
     unittest.main()
